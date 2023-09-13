de3937bf31cc46d76e776b73fb1cf01980fb34a4
Tagg McDonald <taggmcd@gmail.com>
2023-09-12 21:51:17 -0600

    Week1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        float gradePercentage;
+        string letterGrade;
+        string userinput;
+        string passFail;
+
+        Console.Write("What is your grade percentage? ");
+        userinput = Console.ReadLine();
+        gradePercentage = float.Parse(userinput);
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}");
     }
 }
